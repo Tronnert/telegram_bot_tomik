c646efd328271cb25a24c5d62946310b17d8f94d
tronnert <dima.golovanov.2005@mail.ru>
2023-03-02 19:49:02 +0300

    hashtags extract

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from telebot import TeleBot
-from telebot import types
+# from telebot import types
+import re
 
-bot = TeleBot('5967901240:AAEHJhYqKpoZ1l7gv_NosAu50YNbIC9MK-I')
+
+TOKEN = '5967901240:AAEHJhYqKpoZ1l7gv_NosAu50YNbIC9MK-I'
+
+CHANNEL_ID = -1001899049651
+
+bot = TeleBot(TOKEN)
 
 
-@bot.message_handler()
+def extract_hashtags(s):
+    return " ".join(re.findall(r"#(\w+)", s))
+
+
+# @bot.message_handler()
+@bot.channel_post_handler()
 def url(message):
     # markup = types.InlineKeyboardMarkup()
     # btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
     # markup.add(btn1)
     # bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
-    bot.send_message(message.from_user.id, message)
+    # bot.send_message(message.from_user.id, message)
+    if message.sender_chat.id == CHANNEL_ID:
+    # print(message.sender_chat)
+        bot.send_message(message.sender_chat.id, extract_hashtags(message.text))
 
 
-bot.polling(none_stop=True, interval=0) 
+bot.polling(none_stop=True, interval=0)
