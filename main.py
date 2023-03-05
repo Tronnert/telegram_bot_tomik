e80d45b4b8142f405e3317cf444e1054b696db95
tronnert <dima.golovanov.2005@mail.ru>
2023-03-05 11:46:04 +0300

    fffffin

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from os import path
 TOKEN = '5967901240:AAEHJhYqKpoZ1l7gv_NosAu50YNbIC9MK-I'
 
 CHANNEL_ID = -1001783340499
+FORWARDED_CHANNEL_ID = -1001899049651
 CHANNEL_ID_FOR_URLS = "tomikbooks"
 
 
@@ -17,7 +18,6 @@ bot = TeleBot(TOKEN)
 if path.exists("storage.json"):
     with open("storage.json", "r", encoding="UTF-8") as file:
         main_dict = json.load(file)
-        # print(main_dict)
 else:
     main_dict = {    
                 "Книги": 
@@ -43,7 +43,7 @@ else:
                 }
 
 
-def analyse_message(message):
+def analyse_message(message: types.Message, message_id):
     message_entities = filter(lambda x: x.type == "hashtag", message.entities)
     hashtags = []
     for message_entity in message_entities:
@@ -52,35 +52,36 @@ def analyse_message(message):
     book_name = ""
     for hashtag in hashtags:
         if "Т_илл" in hashtag:
-            name = " ".join(filter(lambda x: x != "", hashtag[6:].split("_")))
-            main_dict["Иллюстраторы"][name] = f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"
+            name = " ".join(filter(lambda x: x != "", hashtag[6:].split("_"))).title()
+            main_dict["Иллюстраторы"][name] = f"https://t.me/{CHANNEL_ID_FOR_URLS}/{message_id}"
         elif "Т_кино" in hashtag:
             name = " ".join(filter(lambda x: x != "", hashtag[7:].split("_")))
-            main_dict["Экранизации"][name] = f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"
+            main_dict["Экранизации"][name] = f"https://t.me/{CHANNEL_ID_FOR_URLS}/{message_id}"
         elif "Т_соб" in hashtag:
             name = " ".join(filter(lambda x: x != "", hashtag[6:].split("_")))
-            main_dict["События"][name] = f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"
+            main_dict["События"][name] = f"https://t.me/{CHANNEL_ID_FOR_URLS}/{message_id}"
         elif "Т_лок" in hashtag:
             name = " ".join(filter(lambda x: x != "", hashtag[6:].split("_")))
-            main_dict["Локации"][name] = f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"
+            main_dict["Локации"][name] = f"https://t.me/{CHANNEL_ID_FOR_URLS}/{message_id}"
         elif "Т_кн" in hashtag:
             name = " ".join(filter(lambda x: x != "", hashtag[5:].split("_")))
-            # main_dict["Книги"]["Названия"][name] = f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"
             book_name = name
         elif "Т_авт" in hashtag:
-            # print(hashtag[6:])
             name = " ".join(filter(lambda x: x != "", hashtag[6:].split("_")))
-            main_dict["Книги"]["Авторы"][name] = main_dict["Книги"]["Авторы"].get(name, {}) | {book_name: f"https://t.me/c/{CHANNEL_ID_FOR_URLS}/{message.message_id}"}
+            main_dict["Книги"]["Авторы"][name] = main_dict["Книги"]["Авторы"].get(name, {}) 
+            main_dict["Книги"]["Авторы"][name].update({book_name: f"https://t.me/{CHANNEL_ID_FOR_URLS}/{message_id}"})
     with open("storage.json", "w", encoding="UTF-8") as file:
         json.dump(main_dict, file)
 
 
 @bot.channel_post_handler()
 @bot.edited_channel_post_handler()
-def channel_hand(message):
-    analyse_message(message)
-        # bot.send_message(message.sender_chat.id, " ".join(map(str, hashtags)))
-        # print(main_dict)
+def channel_hand(message: types.Message):
+    # print(message)
+    if message.sender_chat.id == CHANNEL_ID:
+        analyse_message(message, message.message_id)
+    elif message.forward_from_chat != None and message.forward_from_chat.id == CHANNEL_ID and message.sender_chat.id == FORWARDED_CHANNEL_ID:
+        analyse_message(message, message.forward_from_message_id)
 
 
 @bot.message_handler(commands=["start"])
@@ -89,7 +90,7 @@ def search_hand(message: types.Message):
     for key in main_dict.keys():
         btn1 = types.InlineKeyboardButton(text=key, callback_data=key)
         markup.add(btn1)
-    bot.send_message(message.from_user.id, text="Выберите категорию:", reply_markup = markup)
+    bot.send_message(message.from_user.id, text="Выберите категорию:", reply_markup = markup, disable_web_page_preview=True)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -102,7 +103,7 @@ def callback_hand(call: types.CallbackQuery):
             text += f'''<a href="{link}">{name}</a>\n'''
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="main"))
-        bot.edit_message_text(text, chat_id=msg.chat.id, message_id=msg.message_id, parse_mode="HTML", reply_markup=markup)
+        bot.edit_message_text(text, chat_id=msg.chat.id, message_id=msg.message_id, parse_mode="HTML", reply_markup=markup, disable_web_page_preview=True)
     elif "Книги" in action:
         text = f"""{action}:\n"""
         for author, books in sorted(main_dict["Книги"]["Авторы"].items()):
@@ -111,13 +112,13 @@ def callback_hand(call: types.CallbackQuery):
                 text += f'''<a href="{link}">{name}</a>\n'''
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton(text="Назад", callback_data="main"))
-        bot.edit_message_text(text, chat_id=msg.chat.id, message_id=msg.message_id, parse_mode="HTML", reply_markup=markup)
+        bot.edit_message_text(text, chat_id=msg.chat.id, message_id=msg.message_id, parse_mode="HTML", reply_markup=markup, disable_web_page_preview=True)
     elif "main" in action:
         markup = types.InlineKeyboardMarkup()
         for key in main_dict.keys():
             btn1 = types.InlineKeyboardButton(text=key, callback_data=key)
             markup.add(btn1)
-        bot.edit_message_text("Выберите категорию:", chat_id=msg.chat.id, message_id=msg.message_id, reply_markup=markup)
+        bot.edit_message_text("Выберите категорию:", chat_id=msg.chat.id, message_id=msg.message_id, reply_markup=markup, disable_web_page_preview=True)
 
 
 bot.polling(none_stop=True, interval=0)
